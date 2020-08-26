cbbf1601d1ae395a80aa01a13e4acae8877ee550
Adam Hegyi <hegyi.adam@varian.com>
2020-08-26 15:18:42 +0200

    Test init

---
+++ BrowserClient/Tests/BrowserClientTests/TClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserClientTests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class TClass
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
