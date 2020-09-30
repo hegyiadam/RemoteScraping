db924ff675fc9829085401e6e44f955f76e609aa
Adam Hegyi <hegyi.adam@varian.com>
2020-09-30 16:50:36 +0200

    Commands fix

--- Processor/Source/Executable/Executable.py
+++ Processor/Source/Executable/Executable.py
@@ -5,20 +5,26 @@ from Webpage import Webpage
 class Executable(IExecutable):
     def find_tag_by_css_selector(url, css_selector):
         webpage = Webpage(url)
-        find_tag_by_css_selector(webpage,css_selector)
+        Executable.find_tag_by_css_selector_using_webpage(webpage,css_selector)
 
-    def find_tag_by_css_selector(webpage, css_selector):
+    def find_tag_by_css_selector_using_webpage(webpage, css_selector):
         ResultWriter.print_result(webpage.get_element_by_selector(css_selector))
+
     def get_page_numbers(url, page_iteration_selector):
         webpage = Webpage(url)
         return webpage.get_page_numbers(page_iteration_selector)
+
     def do_action_on_iteration(url, page_iteration_selector, iteration_number, action,data):
         webpage = Webpage(url)
         webpage.iterate_through_pages(iteration_number,page_iteration_selector)
         func = getattr(Executable, action)
-        func(webpage,*data)
+        return func(webpage,*data)
+
     def get_iteration_links(url, iteration_selector):
         webpage = Webpage(url)
+        return Executable.get_iteration_links_using_webpage(webpage,iteration_selector)
+
+    def get_iteration_links_using_webpage(webpage, iteration_selector):
         links = webpage.get_iteration_links(iteration_selector)
         return links
     
